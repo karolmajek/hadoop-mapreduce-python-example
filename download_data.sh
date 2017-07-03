b8e4f5d2e15c97c8effb92085b915d06971eb087
Karol Majek <karolmajek@gmail.com>
2017-07-03 15:02:00 +0200

    feat: download data
    #Resolves:
    #See also:
    #########################
    #feat: a new feature
    #fix: a bug fix
    #docs: changes to documentation
    #style: formatting, missing semi colons, etc; no code change
    #refactor: refactoring production code
    #test: adding tests, refactoring test; no production code change
    #chore: updating build tasks, package manager configs, etc; no production code change

---
+++ download_data.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd data
+
+# wget http://content.udacity-data.com/courses/ud617/access_log.gz
+# gunzip access_log.gz
+
+wget http://content.udacity-data.com/courses/ud617/purchases.txt.gz
+gunzip purchases.txt.gz
