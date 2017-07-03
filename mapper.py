1b117e1a304594e739bce2cd1246940858ef28a5
Karol Majek <karolmajek@gmail.com>
2017-07-03 15:15:55 +0200

    item as key instead of store
    #Resolves:
    #See also:
    #########################
    #feat: a new feature
    #fix: a bug fix
    #docs: changes to documentation
    #style: formatting, missing semi colons, etc; no code change
    #refactor: refactoring production code
    #test: adding tests, refactoring test; no production code change
    #chore: updating build tasks, package manager configs, etc; no production code change

--- mapper.py
+++ mapper.py
@@ -22,13 +22,13 @@ def mapper():
         date, time, store, item, cost, payment = data
 
         # Now print out the data that will be passed to the reducer
-        print "{0}\t{1}".format(store, cost)
+        print "{0}\t{1}".format(item, cost)
 
-test_text = """2013-10-09\t13:22\tMiami\tBoots\t99.95\tVisa
-2013-10-09\t13:22\tNew York\tDVD\t9.50\tMasterCard
-2013-10-09 13:22:59 I/O Error
-^d8x28orz28zoijzu1z1zp1OHH3du3ixwcz114<f
-1\t2\t3"""
+# test_text = """2013-10-09\t13:22\tMiami\tBoots\t99.95\tVisa
+# 2013-10-09\t13:22\tNew York\tDVD\t9.50\tMasterCard
+# 2013-10-09 13:22:59 I/O Error
+# ^d8x28orz28zoijzu1z1zp1OHH3du3ixwcz114<f
+# 1\t2\t3"""
 
 # This function allows you to test the mapper with the provided test string
 def main():
