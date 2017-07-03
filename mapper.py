25d268e0f10c0b4e70da1c5e9034cf0016cfd038
Karol Majek <karolmajek@gmail.com>
2017-07-03 15:02:21 +0200

    feat: mapper and reducer
    #Resolves:
    #See also:
    #########################
    #feat: a new feature
    #fix: a bug fix
    #docs: changes to documentation
    #style: formatting, missing semi colons, etc; no code change
    #refactor: refactoring production code
    #test: adding tests, refactoring test; no production code change
    #chore: updating build tasks, package manager configs, etc; no production code change

---
+++ mapper.py
@@ -0,0 +1,40 @@
+# Your task is to make sure that this mapper code does not fail on corrupt data lines,
+# but instead just ignores them and continues working
+import sys
+import StringIO
+
+def mapper():
+    # read standard input line by line
+    for line in sys.stdin:
+        # strip off extra whitespace, split on tab and put the data in an array
+        data = line.strip().split("\t")
+
+        # This is the place you need to do some defensive programming
+        # what if there are not exactly 6 fields in that line?
+        # YOUR CODE HERE
+        if len(data)!=6:
+            continue
+
+        # this next line is called 'multiple assignment' in Python
+        # this is not really necessary, we could access the data
+        # with data[2] and data[5], but we do this for conveniency
+        # and to make the code easier to read
+        date, time, store, item, cost, payment = data
+
+        # Now print out the data that will be passed to the reducer
+        print "{0}\t{1}".format(store, cost)
+
+test_text = """2013-10-09\t13:22\tMiami\tBoots\t99.95\tVisa
+2013-10-09\t13:22\tNew York\tDVD\t9.50\tMasterCard
+2013-10-09 13:22:59 I/O Error
+^d8x28orz28zoijzu1z1zp1OHH3du3ixwcz114<f
+1\t2\t3"""
+
+# This function allows you to test the mapper with the provided test string
+def main():
+	# sys.stdin = StringIO.StringIO(test_text)
+	mapper()
+	sys.stdin = sys.__stdin__
+
+if __name__ == "__main__":
+    main()
