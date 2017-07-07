0d833c8f37a069421e627ddbba0e39c7bbc046aa
Karol Majek <karolmajek@gmail.com>
2017-07-07 09:30:17 +0200

    quiet wget - force owerwrite

--- download_data.sh
+++ download_data.sh
@@ -5,5 +5,5 @@ cd data
 # wget http://content.udacity-data.com/courses/ud617/access_log.gz
 # gunzip access_log.gz
 
-wget http://content.udacity-data.com/courses/ud617/purchases.txt.gz
+wget -q http://content.udacity-data.com/courses/ud617/purchases.txt.gz
 gunzip purchases.txt.gz
